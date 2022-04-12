e2125243edeebed11800c51029ced9851f9ec93e
slowspirit <slowspirit@gmail.com>
2022-04-12 16:21:40 +0300

    added index and univesum pages

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,8 +1,14 @@
 import $ from "jquery";
 
 export default function () {
-    $('.accordion__item').on('click', function () {
-        $(this).children('.accordion__item-text').slideToggle();
-        $(this).children('.accordion__item-title').toggleClass('accordion__item-title--active');
+    $('.accordion__title-box').on('click', function () {
+        // $('.accordion__item').removeClass('accordion__item--active');
+        $(this).siblings('.accordion__text-box').slideToggle();
+        $(this).parent('.accordion__item').toggleClass('accordion__item--active');
+        // $(this).parent('.accordion__item').addClass('accordion__item--active');
+        // $(this).removeClass('accordion__item--active');
+        // $(this).addClass('accordion__item--active');
+        // $(this).children('.accordion__item-text').slideToggle();
+        // $(this).children('.accordion__item-title').toggleClass('accordion__item-title--active');;
     });
 }
